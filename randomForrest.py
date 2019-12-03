10c240f5f3aecb1d27bc6c2ca22c61b32f2e5f42
Cadesh <andrewvrosa@gmail.com>
2019-12-03 15:03:09 -0500

    changed pram to RF

--- randomForrest.py
+++ randomForrest.py
@@ -47,8 +47,8 @@ y = isCateg
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators=20, random_state=0)
+from sklearn.ensemble import RandomForestClassifier
+regressor = RandomForestClassifier(n_estimators=20)
 regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
 
